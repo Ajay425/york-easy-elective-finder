331bf9f82bbf02c1981c638860912c3e3b5ae669
Ajay425 <63657644+Ajay425@users.noreply.github.com>
2025-12-07 02:22:40 -0500

    Update socket.js

--- backend/socket-server/socket.js
+++ backend/socket-server/socket.js
@@ -1,5 +1,6 @@
 import http from "http";
 import { Server } from "socket.io";
+import fs from "fs";
 
 
 const server = http.createServer();
@@ -9,8 +10,29 @@ const io = new Server(server, {
     }
 });
 
-let activeUsers = 0;
+
 let trendingSearches = {};
+const filepath = "./trending.json"
+
+// get current trend searches from the json file
+if (fs.existsSync(filepath)) {
+    try {
+        trendingSearches = JSON.parse(fs.readFileSync(filepath, "utf-8"));
+        console.log("Loaded trending searches from file:", trendingSearches);
+    } catch (err) {
+        console.error("Error reading trending searches file:", err);
+    }
+}
+
+function saveTrending(){
+    fs.writeFileSync(filepath, JSON.stringify(trendingSearches, null, 2));
+}
+
+
+let activeUsers = 0;
+
+
+
 io.on("connection", (socket) => {
     activeUsers++;
     console.log(`A user connected. Active users: ${activeUsers}`);
@@ -23,6 +45,7 @@ io.on("connection", (socket) => {
         const sorted = Object.entries(trendingSearches).sort((a, b) => b[1] - a[1]).slice(0, 5);
 
         const trendingList = sorted.map(([term]) => term);
+        saveTrending();
         io.emit("trendingSearches", trendingList);
 
         console.log("Updated trending searches:", trendingList);
@@ -33,7 +56,13 @@ io.on("connection", (socket) => {
         activeUsers--;
         console.log(`A user disconnected. Active users: ${activeUsers}`);
         io.emit("activeUsers", activeUsers);
+  
     });
+
+    const sorted = Object.entries(trendingSearches).sort((a, b) => b[1] - a[1]).slice(0, 5);
+
+    socket.emit("trendingSearches", sorted);
+
 });
 
 const PORT = process.env.PORT || 4000;
@@ -41,5 +70,3 @@ const PORT = process.env.PORT || 4000;
 server.listen(PORT, () => {
     console.log(`Socket.IO server running at http://localhost:${PORT}/`);
 });
-
-
