ccfcfe0ef2593908cc12bd52ec93c087cdd7ea94
Ajay425 <63657644+Ajay425@users.noreply.github.com>
2025-12-11 18:03:17 -0500

    Update App.jsx

--- frontend/yorku-elective-tracker/src/App.jsx
+++ frontend/yorku-elective-tracker/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -8,7 +8,14 @@ import Home from './components/sections/Home.jsx';
 import Electives from './components/sections/Electives.jsx';
 import ContactUs from './components/sections/Contact.jsx';
 
+
+
 function App() {
+
+    useEffect(() => {
+    // Force Tailwind dark mode globally
+    document.documentElement.classList.add("dark");
+  }, [])
 
   return (
     <div className="min-h-screen bg-[#050509] w-full flex flex-col items-center justify-start">
